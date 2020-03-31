a7ded93cd6cab63712094cc247749a1401e9666d
Vectorex41 <62264547+Vectorex41@users.noreply.github.com>
2020-03-31 08:58:14 -0700

    Reposity TargMasini
    ...

---
+++ Masina.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Targ_masini
+{
+
+    public class Masina
+    {
+        public double model;
+        public int firma;
+        public string culoare;
+        public int an;
+        public double pret;
+        public int [] datat=new int[3];
+        public string [] optiuni=new string [11];
+        
+
+        public Masina()
+        {
+            pret = 0;
+            an = 0;
+            model = 0;
+            firma = 0;
+            culoare = string.Empty;
+            datat[0]=0;
+            datat[1] = 0;
+            datat[2] = 0;
+            optiuni[0] = string.Empty;
+        }
+
+        /*public Masina(string _model, string _firma, string _culoare, int _an, double _pret, List<int> _datat = input.Split(',').ToList(), List<string> _optiuni = input.Split(',').ToList())
+        {
+
+        }*/
+        public void afisare()
+        {
+
+            Console.WriteLine("Masina model {0},culoare {1}, an {2} , pret {3}, datat {4} {5} {6}", model, firma, culoare, an, pret, datat[0],datat[1],datat[2]);
+            Console.Write("Optiuni valabile: ");
+            if (optiuni[0] == string.Empty)
+            {
+                Console.Write("NU exista optiuni pentru aceasta masina");
+            }
+            else
+            {
+                for (int j = 0; j < optiuni.Length; j++)
+                {
+                    Console.Write(optiuni[j] + ", ");
+                }
+            }
+            Console.Write("\nFirma: " + firma);
+        }
+
+        public double getpret() { return pret; }
+        public int getan() { return an; }
+        public int getdataz() { return datat[0]; }
+        public int getdatal() { return datat[1]; }
+        public int getdataa() { return datat[2]; }
+        public double getmodel() { return model; }
+        public int getfirma() { return firma; }
+        public  void setmodel()
+        {
+            Console.WriteLine("\nintroduceti numarul de model");
+            model = Convert.ToDouble(Console.ReadLine());
+        }
+        public  int setfirma()
+        {
+            Console.WriteLine("\nintroduceti firma\n0-Necunoscuta\n1-Ciroen\n2-Suzuki\n3-Honda\n4-Fiat\n5-Volkswagen\n6-BMW\n7-Toyota\n8-Dacia\n9-Ford");
+            firma = Convert.ToInt32(Console.ReadLine());
+            if(firma>9||firma<0)
+            {
+                Console.WriteLine("Introduceti o valoare din cele prezentate");
+                firma = Convert.ToInt32(Console.ReadLine());
+            }
+            return firma;
+        }
+        public  void setculoare()
+        {
+            Console.WriteLine("\nIntroduceti culoarea :");
+            culoare = Console.ReadLine();
+        }
+        public  void setan()
+        {
+            Console.WriteLine("\nIntroduceti anul de fabricare:");
+            an =Convert.ToInt32(Console.ReadLine());
+        }
+        public  void setpret()
+        {
+            Console.WriteLine("\nIntroduceti pretul $:");
+            pret = Convert.ToDouble(Console.ReadLine());
+        }
+        public  void setdatat()
+        {
+            Console.WriteLine("\nintroduceti data tranzactiei (zz,ll,aaaa) :");
+            string lista = Console.ReadLine();
+            string[] data = lista.Split(',');
+            datat[0] = Convert.ToInt32(data[0]);
+            datat[1] = Convert.ToInt32(data[1]);
+            datat[2] = Convert.ToInt32(data[2]);
+        }
+        public  void setop()
+        {
+            Console.WriteLine("\nintroduceti nr de optiuni valabile(max 10) si optiunile cu , :");
+            int o = Convert.ToInt32(Console.ReadLine());
+            for(int j=0;j<o;j++)
+            {
+                Console.Write("\nOptiunea nr." + j);
+                optiuni[j] = Console.ReadLine();
+            }
+        }
+        public string ConversieLaSir()
+        {
+            string[] numef = new string[10];
+            numef[0] = "Necunoscuta";
+            numef[1] = "Citroen";
+            numef[2] = "Suzuki";
+            numef[3] = "Honda";
+            numef[4] = "Fiat";
+            numef[5] = "Volkswagen";
+            numef[6] = "BMW";
+            numef[7] = "Toyota";
+            numef[8] = "Dacia";
+            numef[9] = "Ford";
+            string buff=" ";
+            buff += numef[this.firma];
+            buff += " ";
+            buff += this.model;
+            buff += " ";
+            buff += this.culoare;
+            buff += " din anul";
+            buff += this.an;
+            buff += " la un pret de";
+            buff += this.pret;
+            buff += "$ Tranzactie efectuata pe";
+            buff += this.datat;
+            buff += "\nOptiuni valabile :";
+            if (optiuni[0] == string.Empty)
+            {
+                buff += "NU exista optiuni pentru aceasta masina";
+            }
+            else
+            {
+                for (int j = 0; j < optiuni.Length; j++)
+                { 
+
+                    buff += "-";
+                    buff += optiuni[j];
+                    buff += "\n";
+                }
+            }
+            return buff;
+
+
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targ_masini
+{
+    class Program
+    {
+        public static string[] numef =new string[10];
+        public static int[] firme = new int[10];
+        /*for(int j=0;j<10;j++)
+        {
+            modele[j]=0;
+        }*/
+        public static int i = 0;
+        static void Main(string[] args)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                firme[j] = 0;
+            }
+            Masina m1 = new Masina();
+            m1.afisare();
+            m1.setmodel();
+            m1.ConversieLaSir();//CONVERSIE LA SIR
+            setf();
+            Menu();
+            //int y=10;
+            //string x = "pititci";
+            //Console.WriteLine("Erau odata {0} {1}",y,x);
+        }
+        static void Menu()
+        {
+            Masina[] masini = new Masina[50];
+            for(int j=0;j<50;j++)
+            {
+                masini[j] = new Masina();
+            }
+            int c;
+            string menu = "\nC :Adauga automobile\nA :Afiseaza automobile disponibile\nT :Afiseaza modelul cel mai cautat\nG :Grafic preturi pe o anumita zi\nZ :Tranzactii intr-o anumita zi\nS :Conversie la sir\nI :Info\nX :Exit";
+            Console.WriteLine(menu);
+            do
+            {
+
+                string op = Console.ReadLine();
+                switch (op)
+                {
+                    case "c":
+                        {
+                            
+                            Console.WriteLine("Introduceti numarul de masini pe care doriti sa le adaugati");
+                            c = Convert.ToInt32(Console.ReadLine());
+                            for(int j=0;j<c;j++)
+                            {
+                                try
+                                {
+                                    masini[j].setmodel();
+                                    int v = masini[j].setfirma();
+                                    firme[v]= firme[v]+1;
+                                    Console.WriteLine();
+                                    Console.Write("Indicii curenti ai firmelor :");
+                                    for (int k=0;k<10;k++)
+                                    {
+                                        Console.Write(firme[k]);
+                                    }
+                                    masini[j].setculoare();
+                                    masini[j].setan();
+                                    masini[j].setpret();
+                                    masini[j].setdatat();
+                                    masini[j].setop();
+                                    i++;
+                                }
+                                catch
+                                {
+                                    Console.WriteLine("A aparut o eroare");
+                                }
+                            }
+                            Console.ReadKey();
+                            Console.WriteLine(i + " masini adaugate");
+                            break;
+                        }
+                    case "a":
+                        {
+                            try
+                            {
+                                for (int j = 0; j < i; j++)
+                                {
+                                    masini[j].afisare();
+                                    Console.Write(numef[masini[j].getfirma()]);
+                                }
+                            }
+                            catch
+                            {
+                                Console.WriteLine("Nici o masina inregistrata cu parametrii");
+                            }
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "t":
+                        {
+                            Console.WriteLine("\nModelul ce mai cautat este :" + numef[Max()]);
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "s":
+                        { 
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "g":
+                        { 
+                            //Nu este completat momentan
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "z":
+                        {
+                            int z, l, a,ok=0;
+                            Console.WriteLine("Introduceti ziua");
+                            z = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Introduceti luna");
+                            l = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Introduceti anul");
+                            a = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("S-au gasit :");
+                            for (int j=0;j<i;j++)
+                            {
+                                if (z == masini[j].getdataz() && l == masini[j].getdatal() && a == masini[j].getdataa());
+                                {
+                                    ok = 1;
+                                    masini[j].afisare();
+                                    Console.Write(numef[masini[j].getfirma()]);
+                                }
+                            }
+                            if(ok==0)
+                            {
+                                Console.Write("0 masini cu aceasta data de tranzactie");
+                            }
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "i":
+                        {
+                            Console.WriteLine("Proiect realizat de Ionut Taran");
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "x":
+                        return;
+                }
+                Console.WriteLine("\nAlege o optiune");
+            } while (true);
+        }
+        static void setf()
+        {
+            numef[0] = "Necunoscuta";
+            numef[1] = "Citroen";
+            numef[2] = "Suzuki";
+            numef[3] = "Honda";
+            numef[4] = "Fiat";
+            numef[5] = "Volkswagen";
+            numef[6] = "BMW";
+            numef[7] = "Toyota";
+            numef[8] = "Dacia";
+            numef[9] = "Ford";
+        }
+        static int Max()
+        {
+            int max = 0;
+            for (int j = 0; j < 10; j++)
+            {
+                if (max < firme[j])
+                {
+                    max = j;
+                }
+            }
+            return max;
+        }
+    }
+}
